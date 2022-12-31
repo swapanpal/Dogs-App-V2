949c22e5f773706227b3fa15c5f1eec82339d3af
swapan <swapan.d83@gmail.com>
2022-12-31 20:21:31 +0600

    Implementing Palette Library

---
+++ app/src/main/java/com/example/dogsappv2/model/DogPalette.java
@@ -0,0 +1,9 @@
+package com.example.dogsappv2.model;
+
+public class DogPalette {
+    public int color;
+
+    public DogPalette(int color) {
+        this.color = color;
+    }
+}
--- app/src/main/java/com/example/dogsappv2/view/DetailFragment.java
+++ app/src/main/java/com/example/dogsappv2/view/DetailFragment.java
@@ -1,5 +1,7 @@
 package com.example.dogsappv2.view;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,6 +12,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
+import androidx.palette.graphics.Palette;
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
 
 import android.view.LayoutInflater;
@@ -18,9 +21,13 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.example.dogsappv2.R;
 import com.example.dogsappv2.databinding.FragmentDetailBinding;
 import com.example.dogsappv2.model.DogBreed;
+import com.example.dogsappv2.model.DogPalette;
 import com.example.dogsappv2.util.Util;
 import com.example.dogsappv2.viewmodel.DetailViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -39,40 +46,67 @@ public class DetailFragment extends Fragment {
     public DetailFragment() {
         // Required empty public constructor
     }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment by using DataBinding technology
-        FragmentDetailBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail,container, false);
-            this.binding = binding;
+        FragmentDetailBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false);
+        this.binding = binding;
         return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-      if (getArguments() != null){
-          dogUuid = DetailFragmentArgs.fromBundle(getArguments()).getDogUuid();
-      }
-      viewModel = ViewModelProviders.of(this).get(DetailViewModel.class);
-      viewModel.fetch(dogUuid);
+        if (getArguments() != null) {
+            dogUuid = DetailFragmentArgs.fromBundle(getArguments()).getDogUuid();
+        }
+        viewModel = ViewModelProviders.of(this).get(DetailViewModel.class);
+        viewModel.fetch(dogUuid);
 
         // call the observeViewModel
         observeViewModel();
     }
-    private void observeViewModel(){
+
+    private void observeViewModel() {
         viewModel.dogLiveData.observe(getViewLifecycleOwner(), dogBreed -> {
-            if (dogBreed != null && dogBreed instanceof DogBreed && getContext() != null){
+            if (dogBreed != null && dogBreed instanceof DogBreed && getContext() != null) {
                 binding.setDog(dogBreed);
-
+                if (dogBreed.imageUrl != null) {
+                setupBackgroundColor(dogBreed.imageUrl);
+                }
             }
-
         });
-
     }
+    // Create a method to setup background images by using palette library/ Glide
+    private void setupBackgroundColor(String url){
+        Glide.with(this)
+                .asBitmap()
+                .load(url)
+                .into(new CustomTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                        // Generate the color
+                        Palette.from(resource)
+                                .generate(new Palette.PaletteAsyncListener() {
+                                    @Override
+                                    public void onGenerated(@Nullable Palette palette) {
+                                        // Retrive color .we can set any type Light/Dark...
+                                        int intColor = palette.getLightMutedSwatch().getRgb();
 
+                                        // now create a palette element/Object
+                                        DogPalette myPalette = new DogPalette(intColor);
+                                        // Set the value to the element
+                                        binding.setPalette(myPalette);
+                                    }
+                                });
+                    }
 
+                    @Override
+                    public void onLoadCleared(@Nullable Drawable placeholder) {
+
+                    }
+                });
+    }
 }
